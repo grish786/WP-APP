638005ae537507da82b2d7737c1ddc86a1ae4f54
grish786 <grish786@gmail.com>
2017-07-07 02:51:08 +0530

    Update Missing_Orders.php

--- Missing_Orders.php
+++ Missing_Orders.php
@@ -1,18 +1,1 @@
-declare -a arr=("test-ml")
-
-## now loop through the above array
-for i in "${arr[@]}"
-do
-   echo "$i";
-   echo `git checkout $i`;
-   echo `git pull origin $i`;
-   echo `print mstarunkumar`;
-   echo `Arun4#@!`;
-   echo `git rm -f html/adminer.php`;
-   echo `git add -A`;
-   echo `git commit -m "$i Missing Orders file deleted"`;
-   echo `git push origin $i`;
-   echo `print mstarunkumar`;
-   echo `print Arun4#@!`;
-   # or do whatever with individual element of the array
-done
+test
